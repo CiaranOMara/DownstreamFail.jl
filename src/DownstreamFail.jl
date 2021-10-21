655e0bc6a7581faa1c4378b65c43ce10587f0b17
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2021-10-21 22:08:53 +1100

    Fail scenario

---
+++ src/DownstreamFail.jl
@@ -0,0 +1,5 @@
+module DownstreamFail
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DownstreamFail
+using Test
+
+@testset "Downstream Fail" begin
+    @test true == false
+end
